a72a261d82350f6c46869764fbc6017594882e0f
Iynga Iyngaran <iynga@sas.co.nz>
2022-03-11 10:23:38 +0530

    WIP

--- app/Http/Controllers/PackingListController.php
+++ app/Http/Controllers/PackingListController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Order as OrderResource;
 use App\Models\Order;
 use App\Services\GeneratePackingList;
+use PhpOffice\PhpWord\Settings;
 
 class PackingListController extends Controller
 {
@@ -15,9 +16,9 @@ class PackingListController extends Controller
         $order = (new OrderResource(Order::with('items', 'comments')->where("merch_order_id", $merch_order_id)->first()))->resolve();
         $objGeneratePackingList = new GeneratePackingList($order);
         $packingList = $objGeneratePackingList->generateDoc();
-        dd($packingList);
         $file_name = $merch_order_id . ".docx";
         try {
+            Settings::setZipClass(Settings::PCLZIP);
             $packingList->save(storage_path("packing-list/".$file_name));
         } catch (Exception $e) {
             dd($e);
